72d3efc616b6d6b073758d68c3a71cecd3fa86da
Andrey <medvedevandrey860@gmail.com>
2025-07-09 23:03:38 +0300

    reworkErrMess

--- config/config.go
+++ config/config.go
@@ -17,34 +17,32 @@ func getStrInput(prompt string) string {
 	input, _ := reader.ReadString('\n')
 	return strings.TrimSpace(input)
 }
+
+// функція ініціалізаціі программи
 func Init() {
 	fmt.Println("🔗 URL Shortener")
 	for {
 		fmt.Println("\nВведіть URL для скорочення (або 'exit' для виходу): ")
 		inputs := getStrInput("> ")
 
-		fmt.Println("\n📡 Відправляю запит до TinyURL API...")
-
 		if inputs == "exit" {
 			return
 		}
-
-		// if !checkUrl(inputs) {
-		// 	fmt.Println("❌ Invalid URL")
-		// 	return
-		// }
+		fmt.Println("\n📡 Відправляю запит до TinyURL API...")
 
 		res, err := shorter.ShortingUrl(inputs)
 		if err != nil {
-			fmt.Println("❌ Errors: ", err)
-			return
+			if strings.HasPrefix(err.Error(), "api: ") {
+				fmt.Println("❌ Помилка від TinyURL:", strings.TrimPrefix(err.Error(), "api: "))
+			} else if strings.HasPrefix(err.Error(), "network: ") {
+				fmt.Println("❌ Мережева помилка:", strings.TrimPrefix(err.Error(), "network: "))
+			} else {
+				fmt.Println("❌ Невідома помилка: ", err)
+			}
+			continue
 		}
 		fmt.Println("\n✅ Успішно скорочено!")
 		fmt.Println("🔗 Оригінал: ", inputs)
 		fmt.Println("⭐ Короткий: ", res)
 	}
-}
-
-func checkUrl(url string) bool {
-	return strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "http://")
 }
--- shorter/shorter.go
+++ shorter/shorter.go
@@ -5,22 +5,37 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 func ShortingUrl(originalUrl string) (string, error) {
+
+	if !checkUrl(originalUrl) {
+		return "", fmt.Errorf("api: Error: Invalid URL")
+	}
 
 	originalUrl = url.QueryEscape(originalUrl)
 	apiUrl := "http://tinyurl.com/api-create.php?url=" + originalUrl
 
 	resp, err := http.Get(apiUrl)
 	if err != nil {
-		return "", fmt.Errorf("request failed: %v", err)
+		return "", fmt.Errorf("network: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("failed to read response: %v", err)
+		return "", fmt.Errorf("network: %v", err)
 	}
-	return string(body), nil
+
+	result := string(body)
+
+	if strings.HasPrefix(result, "Error") {
+		return "", fmt.Errorf("api: %s", result)
+	}
+	return result, nil
+}
+
+func checkUrl(url string) bool {
+	return strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "http://")
 }
